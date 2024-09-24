9b95c392bc1d73ef59eba3ac8dd229341538bfef
Benedict JI <saxoji@gmail.com>
2024-09-24 14:12:50 +0900

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -58,7 +58,7 @@ export const Badge = (props: Props) => {
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Linkbricks-AI'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'Linkbricks Horizon-Ai'}</span>
           </a>
         </span>
       </Show>
