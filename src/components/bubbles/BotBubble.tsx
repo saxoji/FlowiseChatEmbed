48caa67016ac56e08275f106dc6fdf3923220790
Benedict JI <saxoji@gmail.com>
2024-10-28 18:14:21 +0900

    Update BotBubble.tsx

--- src/components/bubbles/BotBubble.tsx
+++ src/components/bubbles/BotBubble.tsx
@@ -42,7 +42,7 @@ export const BotBubble = (props: Props) => {
   let botMessageEl: HTMLDivElement | undefined;
   let botDetailsEl: HTMLDetailsElement | undefined;
 
-  Marked.setOptions({ isNoP: true, sanitize: props.renderHTML !== undefined ? !props.renderHTML : true });
+  Marked.setOptions({ isNoP: true });
 
   const [rating, setRating] = createSignal('');
   const [feedbackId, setFeedbackId] = createSignal('');
